99fe254014b1deb322c0cbf5f803a6f0ac5ef59f
PuneetDheri <puneet.dheri@gmail.com>
2023-11-01 18:53:23 -0400

    first_commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PROG20082_Assignment3"
+include(":app")
+ 
